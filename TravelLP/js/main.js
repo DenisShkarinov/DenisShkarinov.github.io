a3e4b38f8698718cb9b5fa39bb413c042519e59a
DenisShkarinov <67820415+DenisShkarinov@users.noreply.github.com>
2020-09-29 14:37:29 +0300

    Created TravelAroundJapanLP

---
+++ TravelLP/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+$('.burger__btn').on('click', function(e) {
+   e.preventDefault();
+   $(this).toggleClass('burger__btn__active');
+   if ($('.burger__btn__active')) {
+      $('.mobile-navigation').toggleClass('mobile-navigation__active');
+   } else {
+      
+   }
+})
+
+$('.modal').on('click', function(e) {
+   e.preventDefault();
+   $('.modal-wrap').toggleClass('modal-wrap__active');
+})
