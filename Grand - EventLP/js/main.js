eeefaccfb88e96273dd8f031f84a9ea29a819923
DenisShkarinov <67820415+DenisShkarinov@users.noreply.github.com>
2020-11-25 19:04:38 +0300

    Add files via upload

--- Grand - EventLP/js/main.js
+++ Grand - EventLP/js/main.js
@@ -2,11 +2,43 @@
 let header=document.querySelector('header');let backToTop=document.querySelector('.back-to__top');window.onscroll=function(){if(window.pageYOffset>=150){header.classList.add('turn__bg');}else{header.classList.remove('turn__bg');}
 if(window.pageYOffset>=350){backToTop.classList.add('show');}else{backToTop.classList.remove('show');}}
 let mySwiper=new Swiper('.swiper-container',{autoplay:{delay:5000,waitForTransition:true},fadeEffect:{crossFade:true},pagination:{el:'.swiper-pagination',clickable:true},navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev',}})
-countdown();function countdown(){let clock=document.querySelectorAll('#clock div');let date=new Date(2021,10,25,12,30,30);let dateNow=new Date();let dateMs=Date.parse('2021-10-25T12:30:30.000-07:00'),dateNowMs=Date.parse(dateNow);let dateDiff=Math.round((dateMs-dateNowMs)/(1000*60*60*24));let days=dateDiff,hours=date.getHours(),minutes=date.getMinutes(),seconds=date.getSeconds();seconds-=1;if(seconds==0){minutes--;if(minutes==0){hours--;if(hours==0){days--;}}}
-clock[0].insertAdjacentHTML('afterbegin',`<p>${dateDiff}</p>`)
-clock[1].insertAdjacentHTML('afterbegin',`<p>${hours}</p>`)
-clock[2].insertAdjacentHTML('afterbegin',`<p>${minutes}</p>`)
-clock[3].insertAdjacentHTML('afterbegin',`<p>${seconds}</p>`)}
+
+const date=new Date(2021,10,25,12,30,30);
+const dateNow=new Date();
+let dateMs=Date.parse('2021-10-25T12:30:30.000-07:00'),
+dateNowMs=Date.parse(dateNow);
+let days = Math.round((dateMs-dateNowMs)/(1000*60*60*24)),
+   hours = 24 - dateNow.getHours(),
+   minutes = 60 - dateNow.getMinutes(),
+   seconds = 60 - dateNow.getSeconds();
+
+(function() {
+   let daysEl = document.querySelector('#clock .days');
+   let hoursEl = document.querySelector('#clock .hours');
+   let minEl = document.querySelector('#clock .minutes');
+   let secEl = document.querySelector('#clock .seconds');
+   
+   let timer = setInterval(function() {
+      secEl.innerHTML = seconds + "\n sec";
+      minEl.innerHTML = minutes + "\n min";
+      hoursEl.innerHTML = hours + "\n hours";
+      daysEl.innerHTML = days + "\n days";
+      --seconds;
+      if (seconds == 0) {
+         seconds = 60;
+         --minutes;
+      }
+      if (minutes == 0) {
+         minutes=60;
+         --hours;
+      }
+      if (hours == 0) {
+         hours = 24;
+         --days;
+      }
+      
+   }, 1000)
+})();
 let links=document.querySelectorAll('.tab li > a');let tabContent=document.querySelectorAll('.tab-content > div');$(links).on('click',function(e){e.preventDefault();$(links).removeClass('active-event')
 $(this).addClass('active-event');let linkAttribute=this.getAttribute('day');for(let item of tabContent){let itemAttr=item.getAttribute('day');if(linkAttribute==itemAttr){$(item).removeClass('hide')}else{$(item).addClass('hide')}}})
 let accordionCard=document.querySelectorAll('#accordion > .card');$('#accordion .card #headingOne').on('click',function(){$('#accordion .card #collapseOne').toggleClass('showCollapse')})
