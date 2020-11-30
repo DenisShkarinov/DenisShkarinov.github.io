8fc0878385a4b2363ea7df6a4fcf4ad7217e5b14
DenisShkarinov <67820415+DenisShkarinov@users.noreply.github.com>
2020-11-30 15:58:49 +0300

    Add files via upload

--- Grand - EventLP/js/main.js
+++ Grand - EventLP/js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-let header=document.querySelector('header');let backToTop=document.querySelector('.back-to__top');window.onscroll=function(){if(window.pageYOffset>=150){header.classList.add('turn__bg');}else{header.classList.remove('turn__bg');}
+const header=document.querySelector('header');const backToTop=document.querySelector('.back-to__top');window.onscroll=function(){if(window.pageYOffset>=150){header.classList.add('turn__bg');}else{header.classList.remove('turn__bg');}
 if(window.pageYOffset>=350){backToTop.classList.add('show');}else{backToTop.classList.remove('show');}}
 let mySwiper=new Swiper('.swiper-container',{autoplay:{delay:5000,waitForTransition:true},fadeEffect:{crossFade:true},pagination:{el:'.swiper-pagination',clickable:true},navigation:{nextEl:'.swiper-button-next',prevEl:'.swiper-button-prev',}})
 
@@ -13,12 +13,12 @@ let days = Math.round((dateMs-dateNowMs)/(1000*60*60*24)),
    seconds = 60 - dateNow.getSeconds();
 
 (function() {
-   let daysEl = document.querySelector('#clock .days');
-   let hoursEl = document.querySelector('#clock .hours');
-   let minEl = document.querySelector('#clock .minutes');
-   let secEl = document.querySelector('#clock .seconds');
+   const daysEl = document.querySelector('#clock .days');
+   const hoursEl = document.querySelector('#clock .hours');
+   const minEl = document.querySelector('#clock .minutes');
+   const secEl = document.querySelector('#clock .seconds');
    
-   let timer = setInterval(function() {
+   const timer = setInterval(function() {
       secEl.innerHTML = seconds + "\n sec";
       minEl.innerHTML = minutes + "\n min";
       hoursEl.innerHTML = hours + "\n hours";
@@ -39,10 +39,10 @@ let days = Math.round((dateMs-dateNowMs)/(1000*60*60*24)),
       
    }, 1000)
 })();
-let links=document.querySelectorAll('.tab li > a');let tabContent=document.querySelectorAll('.tab-content > div');$(links).on('click',function(e){e.preventDefault();$(links).removeClass('active-event')
+const links=document.querySelectorAll('.tab li > a');const tabContent=document.querySelectorAll('.tab-content > div');$(links).on('click',function(e){e.preventDefault();$(links).removeClass('active-event')
 $(this).addClass('active-event');let linkAttribute=this.getAttribute('day');for(let item of tabContent){let itemAttr=item.getAttribute('day');if(linkAttribute==itemAttr){$(item).removeClass('hide')}else{$(item).addClass('hide')}}})
-let accordionCard=document.querySelectorAll('#accordion > .card');$('#accordion .card #headingOne').on('click',function(){$('#accordion .card #collapseOne').toggleClass('showCollapse')})
+const accordionCard=document.querySelectorAll('#accordion > .card');$('#accordion .card #headingOne').on('click',function(){$('#accordion .card #collapseOne').toggleClass('showCollapse')})
 $('#accordion .card #headingTwo').on('click',function(){$('#accordion .card #collapseTwo').toggleClass('showCollapse')})
 $('#accordion .card #headingThree').on('click',function(){$('#accordion .card #collapseThree').toggleClass('showCollapse')})
 $('#accordion .accordion-item .accordion-item-header').on('click',function(){$(this).siblings('.accordion-item-content').toggleClass('show');})
-let burger=document.querySelector('.burger__btn');let mobileMenu=document.querySelector('.mobile-menu__nav');$(burger).on('click',function(e){e.preventDefault();burger.classList.toggle('burger__btn__active');mobileMenu.classList.toggle('mobile-menu__active');$('.mobile-menu__active ul li > a').click(function(){burger.classList.remove('burger__btn__active');mobileMenu.classList.remove('mobile-menu__active');})})
+const burger=document.querySelector('.burger__btn');const mobileMenu=document.querySelector('.mobile-menu__nav');$(burger).on('click',function(e){e.preventDefault();burger.classList.toggle('burger__btn__active');mobileMenu.classList.toggle('mobile-menu__active');$('.mobile-menu__active ul li > a').click(function(){burger.classList.remove('burger__btn__active');mobileMenu.classList.remove('mobile-menu__active');})})
