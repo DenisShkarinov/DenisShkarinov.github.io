5e1155bfd296c58cc1d63b251af48aa9765b6d34
DenisShkarinov <dskarinov@gmail.com>
2020-12-11 20:00:27 +0300

    Skills Section Ready

--- Browny/src/js/common.js
+++ Browny/src/js/common.js
@@ -1,5 +1,23 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-   
+   const skillsBlock = document.getElementById('skills')
+   const progressBars = skillsBlock.querySelectorAll('.progress-bar');
+
+   progressBars.forEach(progress => {
+      let updateProgress = () => {
+         let progressMin = +progress.dataset.initialValue;
+         const progressMax = +progress.dataset.value;
+         
+         if (progressMin < progressMax) {
+            progress.setAttribute('data-initial-value', progressMin+1);
+            progress.style.width += "1%"
+            setTimeout(updateProgress, 1);
+         } else {
+            progressMin = progressMax;
+            progress.style.width = `${progressMin}%`
+         }
+      }
+      updateProgress()
+   })
 })
